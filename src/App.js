110a74679021db7bebcb684c5d09d329fcdef572
neilkapadia7 <neilkapadia7@gmail.com>
2020-02-28 10:52:13 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserForms from './components/UserForms';
+
+function App() {
+  return (
+    <div className="App">
+      <UserForms/>
+    </div>
+  );
+}
+
+export default App;
